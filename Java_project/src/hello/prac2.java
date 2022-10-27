23da0d6b2f7ff56423908d299b4378237889cdbd
nehakale14 <92143651+nehakale14@users.noreply.github.com>
2022-10-27 11:19:59 +0530

    version 6

---
+++ Java_project/src/hello/prac2.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class prac2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=29, n2=18, sum;
+		sum=n1+n2;
+		System.out.println("sum:"+sum);
+
+	}
+
+}
