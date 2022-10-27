db3a1c1de5246a5c275d3215b517b14609ab8ed5
nehakale14 <92143651+nehakale14@users.noreply.github.com>
2022-10-27 11:21:22 +0530

    version 7

---
+++ Java_project/src/hello/prac3.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class prac3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=2, n2=12, sum;
+		sum=n1+n2;
+		System.out.println("sum:"+sum);
+
+
+	}
+
+}
