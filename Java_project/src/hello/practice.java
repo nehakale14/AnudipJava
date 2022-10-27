d724e5af66ecfa4e54c005d337fc16390bb7f6c7
nehakale14 <92143651+nehakale14@users.noreply.github.com>
2022-10-27 11:16:11 +0530

    version 5

--- Java_project/src/hello/practice.java
+++ Java_project/src/hello/practice.java
@@ -1,12 +1,13 @@
 package hello;
 
-public class add {
+public class practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n1=23, n2=12, sum;
 		sum=n1+n2;
 		System.out.println("sum:"+sum);
+
 
 	}
 
