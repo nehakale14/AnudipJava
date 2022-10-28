056c686a46d5378ab4cc5b5c3b3347c17f8208e2
nehakale14 <92143651+nehakale14@users.noreply.github.com>
2022-10-28 12:21:15 +0530

    que1

--- Java_project/src/hello/prac3.java
+++ Java_project/src/hello/prac3.java
@@ -4,7 +4,10 @@ public class prac3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n1=2, n2=12, sum;
+		int n1=2
+				
+				
+				, n2=12, sum;
 		sum=n1+n2;
 		System.out.println("sum:"+sum);
 
